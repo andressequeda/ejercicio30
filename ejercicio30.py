6dade563268401fbc5bec772ba3cf0e90268e70b
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-10 09:46:59 -0500

    Add files via upload

---
+++ ejercicio30.py
@@ -0,0 +1,8 @@
+numero1  =  int ( input ( "Ingrese el primer número:" ))
+numero2  =  int ( input ( "Ingrese el segundo número:" ))
+numero3  =  int ( input ( "Ingrese el tercer número:" ))
+suma  =  numero1  +  numero2
+si  suma  >  numero3 :
+    print ( "La suma de los dos primeros números es mayor que el tercer número ingresado." )
+otra cosa :
+    print ( "La suma de los dos primeros números es menor que el tercer número ingresado." )
